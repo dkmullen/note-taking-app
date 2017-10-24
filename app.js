c35bfca8d54bafb779961115e3c2a34bd274290c
Dennis Mullen <dkmullen@gmail.com>
2017-10-24 13:21:35 -0400

    Add basic logic for adding, reading, removing notes

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
 /*jshint esversion: 6 */
 
-
 console.log('Starting app.js');
 
-// Node's built-in modules - https://nodejs.org/api/
-
 const fs = require('fs'),
-  os = require('os'),
   _ = require('lodash'),
+  // My modules below...
   notes = require('./notes.js');
 
-// _.uniq is a lo-dash utility that strips out the dups in an array
-let filteredArray = _.uniq(['Dennis', 45, 'Capt. Willard', 'Dennis', 78, 45]);
-console.log(filteredArray);
+let command = process.argv[2];
 
-// console.log(_.isString(true)); // _.isString is a lo-dash method
-// console.log(_.isString('Heel it now, dig?'));
-
-// console.log('Result:', notes.add(2,37));
-
-// let res = notes.addNote();
-// console.log(res);
-
-let user = os.userInfo();
-// console.log(user);
-
-// Using an ES6 template string instead of 'Hello ' + etc + '!'
-fs.appendFileSync('greetings.txt', `Hello ${user.username}\n`);
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading a note');
+} else if (command === 'remove') {
+  console.log('Removing a note');
+} else {
+  console.log('Command not recognized');
+}
+ console.log(process.argv[3]);
