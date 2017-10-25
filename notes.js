7e709cc452b3034f1a2c01bb7e78f5ebc2cad5d2
Dennis Mullen <dkmullen@gmail.com>
2017-10-25 11:38:28 -0400

    Make note-adding function

--- notes.js
+++ notes.js
@@ -1,11 +1,41 @@
 /*jshint esversion: 6 */
 console.log('Starting notes.js');
 
+const fs = require('fs'); // fs is built-in to node
+
 // module is a variable available in node js. One of its properties is exports
 // see it with console.log(module);
 
 let addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  let notes = []; // set an array of note objects
+  let note = { // set a note object
+    title,
+    body
+  };
+
+  // try / catch because our file may not exist on the first try
+  try {
+    // Grab the existing file of notes...
+    let notesString = fs.readFileSync('notes-data.json');
+    // ...and parse it into an object
+    notes = JSON.parse(notesString);
+  } catch(e) {
+    // No need to write catch instructions, just define the block
+  }
+
+  // Make an array of notes with the same title, if any
+  let duplicateNotes = notes.filter((note) => note.title === title);
+
+  // The above is the same as this.
+  // let duplicateNotes = notes.filter((note) => {
+  //   return note.title === title; // return notes with a dup title
+  // });
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note); // add the note to the notes array
+    // write to file as a JSON object, create the file if needed
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 let getAll = () => {
   console.log('Getting all notes');
